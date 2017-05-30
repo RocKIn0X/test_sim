c4c0202d393ef74015827902c27c9b4be27d7b92
celloAlbatross <buddhinai.k@ku.th>
2017-05-30 13:41:28 +0700

    find angle

--- test_sim_vision/src/vision_syrena.py
+++ test_sim_vision/src/vision_syrena.py
@@ -4,6 +4,8 @@ import numpy as np
 import rospkg
 import rospy
 from sensor_msgs.msg import CompressedImage
+from test_sim_srv_msg.msg import vision_data.msg
+from test_sim_srv_msg.srv import path_sim.srv
 
 lower_orange = np.array([30,30,152])
 upper_orange = np.array([90,100,255])
@@ -13,6 +15,7 @@ img = None
 contours = None
 orange = None
 closing = None
+
 
 def find_path():
     global contours,img
@@ -38,10 +41,7 @@ def find_path():
 def findColors():
     global contours,img,orange, closing
     kernel = np.ones((5,5),np.uint8)
-    maxX = 0
-    maxY = 0
-    minX = 1000
-    minY = 1000
+
     while not rospy.is_shutdown():
         while img is None:
             print("img None")
@@ -62,23 +62,19 @@ def findColors():
         result = cv2.drawContours(im, contours, -1, (0,255,255), 3)
         # find_path()
         cv2.imshow('result',result)
-        y = 300 - contours[0][0][0][1]
-        x = contours[0][0][0][0] - 400
-        if(x < minX):
-            minX = x
-        if(x > maxX):
-            maxX = x
-        if(y < minY):
-            minY = y
-        if(y > maxY):
-            maxY = y
-        # print('min x: ',minX)
-        # print('max x: ',maxX)
-        # print('min y: ',minY)
-        # print('max y: ',maxY)
-        print(contours[0])
         for c in contours:
             M = cv2.moments(c)
+            rect = (x,y),(ww,hh),angle = cv2.minAreaRect(c)
+            area = ww*hh
+            if area <= 10 or ww <= 2 :
+                continue
+            real_area = cv2.contourArea(c)
+            ratio_area = real_area/area
+            ratio_scale = hh/ww
+
+            angle = 90-Oreintation(M)[0]*180/math.pi
+
+            print("Angle: ", angle)
             if not M['m00'] == 0.0:
                 cx = float(M['m10']/M['m00']) - 400
                 cy = 300 - float(M['m01']/M['m00'])
