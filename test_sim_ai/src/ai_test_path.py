b530880fbad61a458b1d1c0931e2ea97f4744fbe
RocKIn0X <inman27@hotmail.com>
2017-05-31 01:36:21 +0700

    sim can turn_yaw

--- test_sim_ai/src/ai_test_path.py
+++ test_sim_ai/src/ai_test_path.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import rospy
+import math
 from geometry_msgs.msg import Twist
 from std_msgs.msg import Float64, Bool
 
@@ -9,12 +10,16 @@ class PathSim (object):
     def __init__ (self):
         print "Mission : Path"
 
-        #path_srv = 'vision_node'
-        #rospy.wait_for_service (path_srv)
-        #self.detect_path = rospy.ServiceProxy (path_srv, vision_srv)
+        self.angle = 0
+        self.stopt = True
 
         rospy.init_node ('test_sim')
         self.command = rospy.Publisher ('/cmd_vel', Twist, queue_size = 10)
+        self.turn_yaw_rel = rospy.Publisher('/fix/rel/yaw', Float64, queue_size = 10)
+
+        #path_srv = 'vision_node'
+        #rospy.wait_for_service (path_srv)
+        #self.detect_path = rospy.ServiceProxy (path_srv, vision_srv)
 
     def listToTwist (self, list):
         temp = Twist()
@@ -25,6 +30,17 @@ class PathSim (object):
         temp.angular.y = list[4]
         temp.angular.z = list[5]
         return temp
+
+    def stop_turn (self):
+        return self.stopt
+
+    def turn_yaw_relative (self, degree):
+        rad = math.radians (degree)
+        rad = Float64(rad)
+        self.turn_yaw_rel.publish(rad)
+        while not self.stop_turn():
+            rospy.sleep(0.1)
+        print 'turn_yaw_relative'
 
     def pub (self, tw):
         for i in xrange(5):
@@ -39,10 +55,12 @@ class PathSim (object):
         rospy.sleep (time)
 
     def run (self):
+        self.angle = -45
         print 'Start Sim_path Mission'
 
         while (True):
             self.drive ([1, 0, 0, 0, 0, 0])
+            self.turn_yaw_relative(self.angle)
             self.stop (0.5)
 
 if __name__ == '__main__':
